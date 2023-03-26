abee20bb446cf15a55eb2bcd70c224a0012acc78
Fernando C <fercassera@gmail.com>
2023-03-26 08:37:17 -0300

    Creacion de usuarios models

--- category/models.py
+++ category/models.py
@@ -5,7 +5,10 @@ class Category(models.Model):
     category_name = models.CharField(max_length=20, unique = True)
     description = models.CharField(max_length=255, blank = True)
     slug = models.CharField(max_length=100, unique = True)
-    cat_image = models.ImageField(upload_to= 'photos/categories', blank = True)
+    cat_image = models.ImageField(upload_to = 'photos/categories', blank = True)
 
-def __str__(self):
-    return self.category_name
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+    def __str__(self):
+        return self.category_name 
